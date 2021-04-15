2abff46df34c04a84b6f9387ac55debb98dd3265
Avinash <Avinashkumarsinha007@gmail.com>
2021-04-15 18:18:32 +0530

    buttonui

--- naukri-clone-new/src/Components/Home.jsx
+++ naukri-clone-new/src/Components/Home.jsx
@@ -3,13 +3,14 @@ import FirstDiv from './FirstDiv'
 import {Navbar} from "./Navbar"
 import styled from "./Home.module.css"
 import { SpringCog } from './reactSpring/SpringCog'
+
 const Home = () => {
   return (
     <div >
       <Navbar className={styled.display}/>
       <FirstDiv className={styled.display1}></FirstDiv>
       <SpringCog/>
-
+     
     </div>
   )
 }
