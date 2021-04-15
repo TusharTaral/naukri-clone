28bf9d99d38656d8351ccc715a6f2d0ab36a700c
meera <shu@ahb.com>
2021-04-15 12:07:36 +0530

    src

--- naukri-clone-new/src/App.js
+++ naukri-clone-new/src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import { SpringCog } from './Components/reactSpring/SpringCog'
 import { Register } from './Components/Register'
 
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './Components/Home';
 
