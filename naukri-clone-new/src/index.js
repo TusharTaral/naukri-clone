a68ebdbe65e111cb28e11190dc990594a875f5e7
Avinash <Avinashkumarsinha007@gmail.com>
2021-04-15 18:28:06 +0530

    index

--- naukri-clone-new/src/index.js
+++ naukri-clone-new/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import {store} from "./redux/store"
+import {store} from "./redux/store";
+import {BrowserRouter} from "react-router-dom"
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+  <React.StrictMode>  
+    <BrowserRouter>
+      <Provider store={store}>
+       <App />
+      </Provider>
+    </BrowserRouter> 
   </React.StrictMode>,
   document.getElementById('root')
 );
