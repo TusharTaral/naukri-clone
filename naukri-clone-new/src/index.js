4f26fdfda18a5549af80dea5836ae69f093cf743
tushar taral <tushartaral0@gmail.com>
2021-04-16 11:00:38 +0530

    input color change

--- naukri-clone-new/src/index.js
+++ naukri-clone-new/src/index.js
@@ -4,17 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
-import {store} from "./redux/store";
-import {BrowserRouter} from "react-router-dom"
-ReactDOM.render(
-  <React.StrictMode>  
-    <BrowserRouter>
-      <Provider store={store}>
-       <App />
-      </Provider>
-    </BrowserRouter> 
-=======
 import { store } from "./redux/store"
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.render(
@@ -24,7 +13,6 @@ ReactDOM.render(
         <App />
       </Provider>
     </BrowserRouter>
->>>>>>> cb1b6411f50301e8eb2c69c52411ff3afb6fc559
   </React.StrictMode>,
   document.getElementById('root')
 );
